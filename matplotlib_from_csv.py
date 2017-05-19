f736d20c1afb47a19638cb9e5f1b26230d2bc9e2
mbero <mberendt546@gmail.com>
2017-05-19 19:50:40 +0200

    Small modification
    Added feature to read first column from csv file as label

--- matplotlib_from_csv.py
+++ matplotlib_from_csv.py
@@ -4,16 +4,23 @@ import csv
 # example from https://pythonprogramming.net/loading-file-data-matplotlib-tutorial/
 x = []
 y = []
-
+labelx = ''
+labely = ''
 with open('data.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
+    counter = 0
     for row in plots:
-        x.append(int(row[0]))
-        y.append(int(row[1]))
+        if counter == 0:
+            labelx = row[0]
+            labely = row[1]
+        else:
+            x.append(int(row[0]))
+            y.append(int(row[1]))
+        counter += 1
 
-plt.plot(x, y, label='Loaded from file!')
-plt.xlabel('x')
-plt.ylabel('y')
+line1 = plt.plot(x, y, label='Loaded from file!')
+plt.xlabel(labelx)
+plt.ylabel(labely)
 plt.title('Interesting Graph\nCheck it out')
 plt.legend()
 plt.show()
